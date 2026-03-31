8a5336da0d60b1fcdedd168157da5c11495da314
dawoodfaiz <dawoodfaiz.2@gmail.com>
2026-03-31 09:24:41 +0500

    build: install express

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config({ path: "./.env" });
 
-console.log(process.env.APP_NAME);
-console.log(process.env.APP_ENV);
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`${process.env.APP_NAME} app listening on port ${port}`)
+})
+
