f8382b09bbb152bfed7c9de474a8a8fa38d15e5f
dawoodfaiz <dawoodfaiz.2@gmail.com>
2026-04-01 07:09:40 +0500

    build: install cors and setup middleware configurations

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(',') || 'http://localhost:5173',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
