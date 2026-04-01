d742557dfcd58006c9088d2baad2571ec1a86538
dawoodfaiz <dawoodfaiz.2@gmail.com>
2026-04-01 06:39:40 +0500

    refactor: separate app entry point logic from express app logic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
-const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.listen(port, () => {
-  console.log(`${process.env.APP_NAME} app listening on port ${port}`)
-})
-
+    console.log(`${process.env.APP_NAME} app listening on port ${port}`);
+});
